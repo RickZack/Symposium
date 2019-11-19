14a8e1a69834297e9572605d4c87455dd2953a8c
Riccardo Zaccone <riccardozacc@gmail.com>
2019-11-19 20:52:04 +0100

    Update header comment in resourceType.cpp/h

--- resourceType.cpp
+++ resourceType.cpp
@@ -1,6 +1,32 @@
-//
-// Created by akimo on 19/11/2019.
-//
+/*
+* Copyright (c) 2019 Riccardo Zaccone, Ksenia Del Conte Akimova, Alice Morano, Martina Bellissimo
+*
+* This file is part of Symposium.
+* Symposium is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Symposium is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+
+* You should have received a copy of the GNU General Public License
+* along with Symposium.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/*
+ * File:   resourceType.cpp
+ * Project: Symposium
+ * Authors:
+ *          Riccardo Zaccone <riccardo.zaccone at studenti.polito.it>
+ *          Ksenia Del Conte Akimova <s256669 at studenti.polito.it>
+ *          Alice Morano <s259158 at studenti.polito.it>
+ *          Martina Bellissimo <s257307 at studenti.polito.it>
+ *
+ * Created on 19 Novembre 2019, 20.50
+ */
 
 #include "resourceType.h"
 using namespace Symposium;
