94ab76bcd4a5395ccc00b56fc434cfdabddc6ffb
KseniaDelConte <52591791+KseniaDelConte@users.noreply.github.com>
2020-04-05 18:01:13 +0200

    GUI-grafica
    grafica di about

--- GUI/SymposiumGui/mainwindow.cpp
+++ GUI/SymposiumGui/mainwindow.cpp
@@ -18,6 +18,11 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete aboutWindow;
+    delete exitWindow;
+    delete signinWindow;
+    delete signupWindow;
+    delete cl;
 }
 
 
@@ -25,15 +30,38 @@ MainWindow::~MainWindow()
 void MainWindow::on_about_clicked()
 {
     aboutWindow =new about(this);
-    aboutWindow->show();
+    aboutWindow->exec();
 }
 
 void MainWindow::on_exit_clicked()
 {
     QMessageBox msgBox;
     msgBox.setText("    Are you sure to quit??");
+    msgBox.setWindowTitle("Exit");
+    QPixmap pix(":/icon/logo1.png");
+    QIcon p(pix);
+    msgBox.setWindowIcon(p);
     msgBox.setStandardButtons(QMessageBox::Yes| QMessageBox::No);
-    msgBox.setStyleSheet("QMessageBox { background-color: white }");
+    msgBox.button(QMessageBox::Yes)->setObjectName("Yes");
+    msgBox.button(QMessageBox::No)->setObjectName("No");
+    msgBox.setBaseSize(QSize(390, 120));
+    msgBox.setStyleSheet("QMessageBox { background-color:rgb(249, 247, 241); "
+                         "color: rgb(58, 80, 116);"
+                         "font: 14pt 'Baskerville Old Face';} "
+                         "QLabel{color: rgb(58, 80, 116);} "
+                         "QPushButton#Yes { "
+                         "background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1, "
+                         "stop: 0 rgb(95, 167, 175), stop: 1 rgb(58, 80, 116)); "
+                         "color: white; font: 14pt 'Baskerville Old Face'; "
+                         "border-radius:15px; width: 80px; height: 30px; "
+                         "margin-left:50px; margin-right:50px;}"
+                         "QPushButton#No { "
+                         "background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1, "
+                         "stop: 0 rgb(95, 167, 175), stop: 1 grey); "
+                         "color: white; font: 14pt 'Baskerville Old Face'; "
+                         "border-radius:15px; width: 80px; height: 30px; "
+                         "margin-left:50px; margin-right:50px;}");
+    msgBox.setIcon(QMessageBox::Question);
     int ret=msgBox.exec();
 
         if (ret == QMessageBox::Yes)
@@ -64,10 +92,31 @@ void MainWindow::on_SignUp_clicked()
 void MainWindow::closeEvent(QCloseEvent *event)
 {
     QMessageBox msgBox;
-    msgBox.setText("Are you sure to quit??");
+    msgBox.setText("    Are you sure to quit??");
+    msgBox.setWindowTitle("Exit");
+    QPixmap pix(":/icon/logo1.png");
+    QIcon p(pix);
+    msgBox.setWindowIcon(p);
     msgBox.setStandardButtons(QMessageBox::Yes| QMessageBox::No);
+    msgBox.button(QMessageBox::Yes)->setObjectName("Yes");
+    msgBox.button(QMessageBox::No)->setObjectName("No");
     msgBox.setBaseSize(QSize(390, 120));
-    msgBox.setStyleSheet("QMessageBox { background-color:rgb(249, 247, 241); color: rgb(58, 80, 116);font: 14pt 'Baskerville Old Face';} QLabel{color: rgb(58, 80, 116);} QPushButton { background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1, stop: 0 rgb(95, 167, 175), stop: 1 rgb(58, 80, 116)); color: white; font: 14pt 'Baskerville Old Face'; border-radius:15px; width: 80px; height: 30px; margin-left:50px; margin-right:50px;}");
+    msgBox.setStyleSheet("QMessageBox { background-color:rgb(249, 247, 241); "
+                         "color: rgb(58, 80, 116);"
+                         "font: 14pt 'Baskerville Old Face';} "
+                         "QLabel{color: rgb(58, 80, 116);} "
+                         "QPushButton#Yes { "
+                         "background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1, "
+                         "stop: 0 rgb(95, 167, 175), stop: 1 rgb(58, 80, 116)); "
+                         "color: white; font: 14pt 'Baskerville Old Face'; "
+                         "border-radius:15px; width: 80px; height: 30px; "
+                         "margin-left:50px; margin-right:50px;}"
+                         "QPushButton#No { "
+                         "background-color: qlineargradient(x1: 0, y1: 0, x2: 1, y2: 1, "
+                         "stop: 0 rgb(95, 167, 175), stop: 1 grey); "
+                         "color: white; font: 14pt 'Baskerville Old Face'; "
+                         "border-radius:15px; width: 80px; height: 30px; "
+                         "margin-left:50px; margin-right:50px;}");
     msgBox.setIcon(QMessageBox::Question);
     int ret=msgBox.exec();
 
