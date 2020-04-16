a313aa79c192b978d68c0dd251d767afe0f47c47
Riccardo Zaccone <riccardozacc@gmail.com>
2020-04-16 22:16:10 +0200

    Quick fix to removeUser()  - added deletion of user's directory;

--- SymServer.cpp
+++ SymServer.cpp
@@ -329,6 +329,7 @@ void SymServer::removeUser(const std::string &username, const std::string &pwd, 
         throw SymServerException(SymServerException::userWrongPwd, UnpackFileLineFunction());
     closeAllDocsAndPropagateMex(toRemove, workingDoc[username], respMsgId);
     active.erase(username);
+    rootDir->remove(toRemove, "./", username);
     removeRegistered(username);
 
     generateSimpleResponse(userSiteId, msgType::removeUser, respMsgId);
